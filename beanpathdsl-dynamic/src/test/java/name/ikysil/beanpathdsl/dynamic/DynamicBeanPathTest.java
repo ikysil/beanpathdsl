7fe5e1d279d2a4159942048814ee2497666bc242
Illya Kysil <ikysil@ikysil.name>
2017-07-13 00:45:08 +0200

    Add unit tests for DynamicBeanPath

--- beanpathdsl-dynamic/src/test/java/name/ikysil/beanpathdsl/dynamic/DynamicBeanPathTest.java
+++ beanpathdsl-dynamic/src/test/java/name/ikysil/beanpathdsl/dynamic/DynamicBeanPathTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Illya Kysil <ikysil@ikysil.name>.
+ * Copyright 2017 Illya Kysil <ikysil@ikysil.name>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package name.ikysil.beanpathdsl.sandbox.dynamic;
+package name.ikysil.beanpathdsl.dynamic;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -22,35 +22,47 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.xml.datatype.XMLGregorianCalendar;
+import org.junit.Test;
 
 import static name.ikysil.beanpathdsl.dynamic.DynamicBeanPath.expr;
 import static name.ikysil.beanpathdsl.dynamic.DynamicBeanPath.path;
 import static name.ikysil.beanpathdsl.dynamic.DynamicBeanPath.root;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
 
 /**
  *
  * @author Illya Kysil <ikysil@ikysil.name>
  */
-public class Main {
+public class DynamicBeanPathTest {
 
-    public static void main(String[] args) {
-        System.out.println(path(root(TestBeanA.class).getStrings().isEmpty()));
-        System.out.println(path(root(TestBeanA.class).getArray()));
-        System.out.println(path(root(TestBeanA.class).get2DArray()));
-        System.out.println(path(root(TestBeanA.class).get3DArray()));
-        System.out.println(path(root(TestBeanA.class).getLong().longValue()));
-        System.out.println(path(root(TestBeanA.class).getInteger()));
-        System.out.println(path(root(TestBeanA.class).getChild()));
-        System.out.println(path(root(TestBeanA.class).getChild().getParent().getChild().getGrandChild()));
+    @Test
+    public void successWhenNavigating() {
+        System.out.println("successWhenNavigating");
+        assertThat(path(root(TestBeanA.class).getStrings().isEmpty()), equalTo("strings.empty"));
+        assertThat(path(root(TestBeanA.class).getArray()), equalTo("array"));
+        assertThat(path(root(TestBeanA.class).get2DArray()), equalTo("2DArray"));
+        assertThat(path(root(TestBeanA.class).get3DArray()), equalTo("3DArray"));
+        // java.lang.Long is final
+        assertThat(path(root(TestBeanA.class).getLong().longValue()), equalTo("long"));
+        assertThat(path(root(TestBeanA.class).getInteger()), equalTo("integer"));
+        assertThat(path(root(TestBeanA.class).getChild()), equalTo("child"));
+        assertThat(path(root(TestBeanA.class).getChild().getParent().getChild().getGrandChild()), equalTo("child.parent.child.grandChild"));
         root(TestBeanA.class).setData(Collections.EMPTY_LIST);
-        System.out.println(path());
-        System.out.println(path(expr(JButton.class).getAction().isEnabled()));
-        System.out.println(path(root(JLabel.class).getAccessibleContext().getAccessibleAction()));
-        System.out.println(path(root(TestBeanA.class).toString()));
-        System.out.println(path(root(JLabel.class).toString()));
-        System.out.println(path(root(TestBeanA.class).getChild().getCalendar().getTimeInMillis()));
-        System.out.println(path(root(TestBeanA.class).getCalendar().getEon().getLowestSetBit()));
-        System.out.println(path(root(TestBeanA.class).getClass().getAnnotations()));
+        assertThat(path(), equalTo("data"));
+        // expr() carries over the existing path
+        assertThat(path(expr(JButton.class).getAction().isEnabled()), equalTo("data.action.enabled"));
+        assertThat(path(root(JLabel.class).getAccessibleContext().getAccessibleAction()), equalTo("accessibleContext.accessibleAction"));
+        assertThat(path(root(TestBeanA.class).getChild().getCalendar().getTimeInMillis()), equalTo("child.calendar.timeInMillis"));
+        // getEon() returns java.math.BigInteger which has no no-arg constructor
+        assertThat(path(root(TestBeanA.class).getCalendar().getEon().getLowestSetBit()), equalTo("calendar.eon"));
+        // java.lang.Class is final
+        assertThat(path(root(TestBeanA.class).getClass().getAnnotations()), nullValue());
+        // TestBeanA.toString() is not overriden
+        assertThat(path(root(TestBeanA.class).toString()), nullValue());
+        // JLabel.toString() invokes getName()
+        assertThat(path(root(JLabel.class).toString()), equalTo("name"));
     }
 
     public static abstract class TestBeanA {
